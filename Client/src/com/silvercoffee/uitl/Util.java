0bae9e29bb4bbc50e6c17cff34c7f4b55c2bcda5
Marco Costa <marcotuliocosta@live.com>
2018-01-22 01:55:17 -0200

    Criação metodo de conversão de polar para retangular

--- Client/src/com/silvercoffee/uitl/Util.java
+++ Client/src/com/silvercoffee/uitl/Util.java
@@ -6,12 +6,16 @@ import org.newdawn.slick.geom.Point;
 public class Util {
 
     public static double scalarProduct(Point u, Point v) {
-        return u.getX()*v.getX()+u.getY()+v.getY();
+        return u.getX() * v.getX() + u.getY() + v.getY();
     }
 
 
-    public static double module(Point u){
-        return  Math.sqrt(Math.pow(u.getX(),2)+Math.pow(u.getY(),2));
+    public static double module(Point u) {
+        return Math.sqrt(Math.pow(u.getX(), 2) + Math.pow(u.getY(), 2));
+    }
+
+    public static Point polToRec(double angle, double amplitude) {
+        return new Point((float) (Math.cos(angle) * amplitude), (float) (Math.sin(angle) * amplitude)   );
     }
 
 }
